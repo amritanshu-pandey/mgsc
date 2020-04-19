8608b88d7b09906a2353a4f04117995f0d549d95
Amritanshu Pandey <amp.msit@gmail.com>
2020-04-19 15:55:43 +1000

    initial version

--- mgsc/cli.py
+++ mgsc/cli.py
@@ -6,8 +6,7 @@ import click
 @click.command()
 def main(args=None):
     """Console script for mgsc."""
-    click.echo("Replace this message by putting your code into "
-               "mgsc.cli.main")
+    click.echo("Replace this message by putting your code into " "mgsc.cli.main")
     click.echo("See click documentation at https://click.palletsprojects.com/")
     return 0
 
